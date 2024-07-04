0ab4582e1bcbc5c96067aeaff1d571e1bc14af37
Ahmad Mzayek <ahmadmzayek.cs@gmail.com>
2024-07-04 11:48:07 +0300

    Added Documentation to 'TextColors.h'

--- include/TextColors.h
+++ include/TextColors.h
@@ -1,7 +1,14 @@
 /*
+    @headerfile     TextColors.h
     @author         Ahmad Mzayek
     @date           Thursday, July 4th, 2024
-    @headerfile     TextColors.h
+    @brief          This header file defines Macros for Colored Text Output in the terminal.
+    @details        The Macros defined in this file can be used to print text in various colors and styles
+    @details        using ANSI escape codes. Each color has both a Regular and a Bold version.
+    @example        fprintf(stdout, RED "This text is red." DEFAULT_COLOR "\n");
+    @note           Ensure to reset the text color to the default color using 'DEFAULT_COLOR' after using
+    @note           any color macro to avoid unwanted text color changes in subsequent output.
+    @note           Compatibility is NOT guaranteed across all terminal emulators and systems.
 */
 
 #ifndef __TEXT_COLORS_H_
